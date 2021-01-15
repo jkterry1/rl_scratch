ffce498d10e15247291bc070b3d2c926c54d57b6
Justin Terry <justinkterry@gmail.com>
2021-01-15 13:43:00 -0500

    fix supersuit

--- run.py
+++ run.py
@@ -8,8 +8,14 @@ from pettingzoo.utils import save_observation
 
 env = pistonball_v3.env(n_pistons=6, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
 
-#env = ss.frame_stack_v1(ss.normalize_obs_v0(ss.resize_v0(ss.dtype_v0(ss.color_reduction_v0(env, mode='B'),'float32'), x_size=20, y_size=76)), 3)
-
-env = ss.normalize_obs_v0(ss.resize_v0(ss.dtype_v0(ss.color_reduction_v0(env, mode='B'),'float32'), x_size=20, y_size=76)), 3
+print(type(env))
+env = ss.color_reduction_v0(env, mode='B')
+print(type(env))
+env = ss.dtype_v0(env,'float32')
+print(type(env))
+env = ss.resize_v0(env, x_size=20, y_size=76)
+print(type(env))
+# env = ss.frame_stack_v1(env,1)
+# print(type(env))
 
 save_observation(env, all_agents=True)
