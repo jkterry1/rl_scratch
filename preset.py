3aa43049102a7f1ccc241480c61439b59d335471
Justin Terry <justinkterry@gmail.com>
2021-01-15 21:29:54 -0500

    fix action spaces

--- preset.py
+++ preset.py
@@ -69,7 +69,7 @@ class PPOContinuousPreset(Preset):
         self.value_model = hyperparameters["value_model_constructor"].to(device)
         self.policy_model = hyperparameters["policy_model_constructor"].to(device)
         self.device = device
-        self.action_space = env.action_space
+        self.action_space = env.action_spacees['piston_0']
         self.hyperparameters = hyperparameters
 
     def agent(self, writer=DummyWriter(), train_steps=float('inf')):
