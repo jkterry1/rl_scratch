c0f4f6d2d680ad6be0047f8e6f4682a35573dcdd
Justin Terry <justinkterry@gmail.com>
2021-01-25 20:47:58 -0500

    switch to sac

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from preset import ppo
+from preset2 import sac
 from all.experiments import run_experiment
 # from all.experiments.multiagent_env_experiment import MultiagentEnvExperiment
 # from all.environments import GymEnvironment
@@ -16,53 +16,58 @@ env = ss.flatten_v0(env)
 env = ss.frame_stack_v1(env, 1)
 
 state_dim = env.observation_spaces['piston_0'].shape[0]
-action_dim = 2  # single channel PPO
-hidden1 = 128
-hidden2 = 64
+action_dim = 1  # single channel PPO
+hidden1 = 400
+hidden2 = 300
 
-v = nn.Sequential(
-    nn.Linear(state_dim, hidden1),
-    nn.ReLU(),
-    nn.Linear(hidden1, hidden2),
-    nn.ReLU(),
-    nn.Linear(hidden2, 1)
-)
 
-policy = nn.Sequential(
-    nn.Linear(state_dim, hidden1),
-    nn.ReLU(),
-    nn.Linear(hidden1, hidden2),
-    nn.ReLU(),
-    nn.Linear(hidden2, action_dim*2)
+def fc_v():
+    return nn.Sequential(
+        nn.Linear(state_dim, hidden1),
+        nn.ReLU(),
+        nn.Linear(hidden1, hidden2),
+        nn.ReLU(),
+        nn.Linear(hidden2, 1)
+    )
+
+
+def fc_soft_policy():
+    return nn.Sequential(
+        nn.Linear(state_dim, hidden1),
+        nn.ReLU(),
+        nn.Linear(hidden1, hidden2),
+        nn.ReLU(),
+        nn.Linear(hidden2, action_dim*2)
     )
 
 
 hyperparameters = {
     # Common settings
-    "discount_factor": 0.99,
+    "discount_factor": 0.98,
     # Adam optimizer settings
-    "lr": 2.5e-4,  # Adam learning rate
-    "eps": 1e-5,  # Adam stability
-    # Loss scaling
-    "entropy_loss_scaling": 0.01,
-    "value_loss_scaling": 0.5,
+    "lr_q": 1e-3,
+    "lr_v": 1e-3,
+    "lr_pi": 1e-4,
     # Training settings
-    "clip_grad": 0.5,
-    "clip_initial": 0.1,
-    "clip_final": 0.01,
-    "epochs": 4,
-    "minibatches": 4,
-    # Batch settings
-    "n_envs": 1,
-    "n_steps": 128,
-    # GAE settings
-    "lam": 0.95,
+    "minibatch_size": 100,
+    "update_frequency": 2,
+    "polyak_rate": 0.005,
+    # Replay Buffer settings
+    "replay_start_size": 5000,
+    "replay_buffer_size": 1e6,
+    # Exploration settings
+    "temperature_initial": 0.1,
+    "lr_temperature": 1e-5,
+    "entropy_target_scaling": 1.,
     # Model construction
-    "value_model_constructor": v,
-    "policy_model_constructor": policy,
+    "q1_model_constructor": fc_v,
+    "q2_model_constructor": fc_v,
+    "v_model_constructor": fc_v,
+    "policy_model_constructor": fc_soft_policy
 }
 
-run_experiment([ppo(hyperparameters=hyperparameters)], [MultiagentPettingZooEnv(env ,'pistonball')], frames=5e6)
+
+run_experiment([sac(hyperparameters=hyperparameters)], [MultiagentPettingZooEnv(env ,'pistonball')], frames=5e6)
 
 # env.reset()
 # save_observation(env, all_agents=True)
