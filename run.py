212aea90cfed462fadb5e00f030a4b6ada97c5f5
Justin Terry <justinkterry@gmail.com>
2021-01-28 20:45:41 -0500

    +1

--- run.py
+++ run.py
@@ -4,18 +4,17 @@ from all.experiments.multiagent_env_experiment import MultiagentEnvExperiment
 from all.environments import MultiagentPettingZooEnv
 import supersuit as ss
 
-"""
+
 from pettingzoo.butterfly import pistonball_v3
 
 env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
-
 env = ss.color_reduction_v0(env, mode='B')
 env = ss.dtype_v0(env, 'float32')
 env = ss.resize_v0(env, x_size=20, y_size=76)
 env = ss.flatten_v0(env)
 env = ss.frame_stack_v1(env, 2)
-"""
 
+"""
 from pettingzoo.butterfly import cooperative_pong_v2
 
 env = cooperative_pong_v2.env()
@@ -24,7 +23,7 @@ env = ss.dtype_v0(env, 'float32')
 env = ss.resize_v0(env, x_size=40, y_size=40)
 env = ss.flatten_v0(env)
 env = ss.frame_stack_v1(env, 2)
-
+"""
 
 state_dim = env.observation_spaces['piston_0'].shape[0]
 action_dim = 1  # single channel PPO
@@ -34,7 +33,7 @@ hidden2 = 300
 
 def fc_v(env):
     return nn.Sequential(
-        nn.Linear(state_dim, hidden1),
+        nn.Linear(state_dim+1, hidden1),
         nn.ReLU(),
         nn.Linear(hidden1, hidden2),
         nn.ReLU(),
@@ -74,7 +73,7 @@ hyperparameters = {
     "update_frequency": 2,
     "polyak_rate": 0.005,
     # Replay Buffer settings
-    "replay_start_size": 5000,
+    "replay_start_size": 300,  # was 5000
     "replay_buffer_size": 1e6,
     # Exploration settings
     "temperature_initial": 0.1,
@@ -101,4 +100,3 @@ experiment.save()
 experiment.close()
 
 # supersuit obs size logging
-# 
