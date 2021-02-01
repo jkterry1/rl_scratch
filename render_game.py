62c1f6396972faff139d34f9cd5f8ed14e964f38
Justin Terry <justinkterry@gmail.com>
2021-01-31 23:56:08 -0500

    fix environment

--- render_game.py
+++ render_game.py
@@ -18,8 +18,8 @@ checkpoint_path = "/home/justinkterry/ray_results/pistonball_v3/PPO/PPO_pistonba
 ModelCatalog.register_custom_model("MLPModelV2", MLPModelV2)
 
 
-def env_creator():
-    env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
+def env_creator(args):
+    env = pistonball_v3.env(n_pistons=20, local_ratio=0, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
     env = ss.color_reduction_v0(env, mode='B')
     env = ss.dtype_v0(env, 'float32')
     env = ss.resize_v0(env, x_size=20, y_size=76)
@@ -27,6 +27,7 @@ def env_creator():
     env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
     env = ss.frame_stack_v1(env, 3)
     return env
+
 
 
 env = env_creator()
@@ -43,7 +44,6 @@ PPOagent.restore(checkpoint_path)
 
 reward = 0
 obs_list = []
-iteration = 0
 i = 0
 env.reset()
 
