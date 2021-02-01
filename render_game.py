82d957e98a726ff59cd95ca47987ddc840aabc3d
Justin Terry <justinkterry@gmail.com>
2021-01-31 19:20:32 -0500

    fix things

--- render_game.py
+++ render_game.py
@@ -2,7 +2,6 @@ import ray
 import os
 import pickle
 from ray.rllib.models import ModelCatalog
-from run_rllib import MLPModelV2
 from ray.tune.registry import register_trainable, register_env
 from ray.rllib.agents.dqn import ApexTrainer
 from ray.rllib.agents.ppo import PPOTrainer
@@ -15,88 +14,48 @@ from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
 # path should end with checkpoint-<> data file
 checkpoint_path = "~/ray_results/pistonball_v3/PPO/PPO_pistonball_v3_19368_00000_0_2021-01-30_20-45-33/checkpoint_100/checkpoint-100"
 
-"""
-def make_env_creator():
-    def env_creator(args):
-        env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
-        env = ss.color_reduction_v0(env, mode='B')
-        env = ss.dtype_v0(env, 'float32')
-        env = ss.resize_v0(env, x_size=20, y_size=76)
-        env = ss.flatten_v0(env)
-        env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
-        env = ss.frame_stack_v1(env, 3)
-        return env
-    return env_creator
+
+def env_creator(args):
+    env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
+    env = ss.color_reduction_v0(env, mode='B')
+    env = ss.dtype_v0(env, 'float32')
+    env = ss.resize_v0(env, x_size=20, y_size=76)
+    env = ss.flatten_v0(env)
+    env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
+    env = ss.frame_stack_v1(env, 3)
+    return env
 
 
-ModelCatalog.register_custom_model("MLPModelV2", MLPModelV2)
-
-env_creator = make_env_creator()
-
+env = env_creator()
 env_name = "pistonball_v3"
-
-register_env(env_name, lambda config: PettingZooEnv(env_creator(config)))
-"""
-
-env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
-env = ss.color_reduction_v0(env, mode='B')
-env = ss.dtype_v0(env, 'float32')
-env = ss.resize_v0(env, x_size=20, y_size=76)
-env = ss.flatten_v0(env)
-env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
-env = ss.frame_stack_v1(env, 3)
+register_env(env_name, lambda config: PettingZooEnv(env_creator()))
 
 
-print('-----------------preinit----------------')
-#ray.init()
-print('-----------------postinit----------------')
-
-config = DEFAULT_CONFIG.copy()
-
-ModelCatalog.register_custom_model("MLPModelV2", MLPModelV2)
-obs_space = env.observation_spaces[env.possible_agents[0]]
-act_space = env.action_spaces[env.possible_agents[0]]
-
-
-def gen_policy(i):
-    config = {
-        "model": {
-            "custom_model": "MLPModelV2",
-        },
-        "gamma": 0.99,
-    }
-    return (None, obs_space, act_space, config)
+def env_creator():
+    env = pistonball_v3.env(n_pistons=10, local_ratio=0.2, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=900)
+    env = ss.color_reduction_v0(env, mode='B')
+    env = ss.dtype_v0(env, 'float32')
+    env = ss.resize_v0(env, x_size=20, y_size=76)
+    env = ss.flatten_v0(env)
+    env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
+    env = ss.frame_stack_v1(env, 3)
+    return env
 
 
-policies = {"policy_0": gen_policy(0)}
-policy_ids = list(policies.keys())
+env = env_creator()
 
-config["multiagent"] = {"policies": policies, "policy_mapping_fn": (lambda agent_id: policy_ids[0])}
 
-"""
-cls = get_trainable_cls()
-agent = cls(env=pistonball_v3, config=config)
-agent.restore(checkpoint_path)
-"""
+config_path = os.path.dirname(checkpoint_path)
+config_path = os.path.join(config_path, "../params.pkl")
+with open(config_path, "rb") as f:
+    config = pickle.load(f)
 
-print('-----------------pretrainer----------------')
-agent = PPOTrainer(env=pistonball_v3, config=config)
-print('-----------------posttrainer----------------')
+
+agent = PPOTrainer(env='pistonball_v3', config=config)
 agent.restore(checkpoint_path)
 
 print('to playthrough')
 
-"""
-# init obs, action, reward
-env.reset()
-rewards, action_dict = {}, {}
-for agent_id in env.agent_ids:
-    assert isinstance(agent_id, int), "Error: agent_ids are not ints."
-    # action_dict = dict(zip(env.agent_ids, [np.array([0,1,0]) for _ in range(len(env.agent_ids))])) # no action = [0,1,0]
-    rewards[agent_id] = 0
-
-totalReward = 0
-"""
 done = False
 
 reward = 0
@@ -124,7 +83,7 @@ while not done:
 print('playthrough over')
 env.close()
 print(reward)
-write_gif(obs_list)
+write_gif(obs_list, 'pistonball.gif')
 #print("done", done, totalReward)
 
 # look into reward
