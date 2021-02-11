c9de2d6d74ce381593213e5a11edb086064724d5
Justin Terry <justinkterry@gmail.com>
2021-02-11 14:19:25 -0500

    sdl issue

--- ben_manual.py
+++ ben_manual.py
@@ -1,6 +1,8 @@
 from pettingzoo.butterfly import pistonball_v3
 import numpy as np
 from array2gif import write_gif
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 GRAYSCALE_WEIGHTS = np.array([0.299, 0.587, 0.114], dtype=np.float32)
 
