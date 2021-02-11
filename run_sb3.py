48fce8ac7f7dd7831b08bcb37cbb42a1d39a5cb9
Justin Terry <justinkterry@gmail.com>
2021-02-11 15:49:35 -0500

    possibly working sb

--- run_sb3.py
+++ run_sb3.py
@@ -1,4 +1,5 @@
-from stable_baselines3 import PPO
+from stable_baselines.common.policies import CnnPolicy
+from stable_baselines import PPO2
 from pettingzoo.butterfly import pistonball_v3
 import supersuit as ss
 
@@ -7,8 +8,16 @@ env = ss.color_reduction_v0(env, mode='B')
 env = ss.dtype_v0(env, 'float32')
 env = ss.resize_v0(env, x_size=84, y_size=84)
 env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
+env = ss.frame_stack_v1(env, 4)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
-env = ss.concat_vec_envs_v0(env, 4, base_class='stable_baselines3')
+env = ss.concat_vec_envs_v0(env, 4, base_class='stable_baselines')
 
-model = PPO('CnnPolicy', env, verbose=3, n_steps=16)
+model = PPO2('CnnPolicy', env, verbose=3, n_steps=16)
 model.learn(total_timesteps=1000000)
+
+
+
+
+"""
+Replace with custom CNN to remove preprocessing overhead
+"""
