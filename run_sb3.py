cf61a636e1c639539c207bbb89de861d70b50a8a
Justin Terry <justinkterry@gmail.com>
2021-02-11 20:26:19 -0500

    determined number of steps

--- run_sb3.py
+++ run_sb3.py
@@ -10,19 +10,20 @@ env = ss.resize_v0(env, x_size=84, y_size=84)
 env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
 env = ss.frame_stack_v1(env, 3)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
-env = ss.concat_vec_envs_v0(env, 16, num_cpus=4, base_class='stable_baselines')
+env = ss.concat_vec_envs_v0(env, 8, num_cpus=4, base_class='stable_baselines')
 
-model = PPO2(CnnPolicy, env, verbose=3, gamma=0.99, n_steps=128, ent_coef=0.01, learning_rate=0.00025, vf_coef=0.5, max_grad_norm=0.5, lam=0.95, nminibatches=4, noptepochs=4, cliprange=0.2, cliprange_vf=1)
-model.learn(total_timesteps=1000000)
+model = PPO2(CnnPolicy, env, verbose=3, gamma=0.99, n_steps=125, ent_coef=0.01, learning_rate=0.00025, vf_coef=0.5, max_grad_norm=0.5, lam=0.95, nminibatches=4, noptepochs=4, cliprange=0.2, cliprange_vf=1)
+model.learn(total_timesteps=2000000)  # half convergence time in my rllib tests
 
 
 
 
 """
-Figure out number of envs thing
 SB rendering code
+SB policy saving
+Turn on logging?
 
-Tune number of CPUs, number of minibatches,
+Tune number of minibatches
 
 Figure out CPU/RAM utilization is
 Hook into Tune
@@ -30,7 +31,6 @@ Get tune set to run on many GCP instances
 Figure out minibatch size
 Little search
 
-gamma=0.99, n_steps=128, ent_coef=0.01, learning_rate=0.00025, vf_coef=0.5, max_grad_norm=0.5, lam=0.95, nminibatches=4, noptepochs=4, cliprange=0.2, cliprange_vf=None,
 
 Future:
 ent coeff schedule
@@ -38,4 +38,11 @@ Orthogonal policy initialization
 Check VF sharing is on
 Seriously look into LSTMs/GRUs/etc.
 Adam annealing
+
+16 -> 3GB GPU, 4GB RAM
+128-> maxed GPU, 10GB CPU
+
+
+magic number: 4 minibatches, 
+
 """
