8934eadbb9cbd7a36eab83d830da9649af9f1856
Justin Terry <justinkterry@gmail.com>
2021-02-24 22:09:00 -0500

    shorter timesteps

--- run_hyperparameters.py
+++ run_hyperparameters.py
@@ -83,7 +83,8 @@ def train(parameterization):
 
     env = make_env(parameterization['n_envs'])
     model = PPO2(CnnPolicy, env, gamma=parameterization['gamma'], n_steps=parameterization['n_steps'], ent_coef=parameterization['ent_coef'], learning_rate=parameterization['learning_rate'], vf_coef=parameterization['vf_coef'], max_grad_norm=parameterization['max_grad_norm'], lam=parameterization['lam'], nminibatches=nminibatches, noptepochs=parameterization['noptepochs'], cliprange_vf=parameterization['cliprange_vf'])
-    model.learn(total_timesteps=2000000, callback=checkpoint_callback)
+    #model.learn(total_timesteps=2000000, callback=checkpoint_callback)
+    model.learn(total_timesteps=10000, callback=checkpoint_callback)
     mean_reward = evaluate_all_policies(folder)
     tune.report(negative_mean_reward=mean_reward)
 
