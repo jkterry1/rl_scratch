790368ac5edd3c8e29cef00050f58c5aa6d5084b
Justin Terry <justinkterry@gmail.com>
2021-03-06 18:43:33 -0500

    readd console logging

--- run_hyperparameters.py
+++ run_hyperparameters.py
@@ -89,10 +89,14 @@ def train(parameterization):
     nminibatches = int(batch_size/divisors[-1])
 
     env = make_env(parameterization['n_envs'])
+    print('envs made')
     model = PPO2(CnnPolicy, env, gamma=parameterization['gamma'], n_steps=parameterization['n_steps'], ent_coef=parameterization['ent_coef'], learning_rate=parameterization['learning_rate'], vf_coef=parameterization['vf_coef'], max_grad_norm=parameterization['max_grad_norm'], lam=parameterization['lam'], nminibatches=nminibatches, noptepochs=parameterization['noptepochs'], cliprange_vf=parameterization['cliprange_vf'], tensorboard_log=('/home/justin_terry/tensorboard_logs/' + name + '/'))
+    print('model loaded')
     model.learn(total_timesteps=2000000, callback=checkpoint_callback)  # not off by factor of number of agents
+    print('model learned')
     mean_reward = evaluate_all_policies(folder)
     tune.report(mean_reward=mean_reward)
+
 
 
 ray.init(address='auto')
@@ -100,8 +104,8 @@ ray.init(address='auto')
 
 analysis = tune.run(
     train,
-    num_samples=8,
-    search_alg=AxSearch(ax_client=ax, max_concurrent=4),
+    num_samples=4,
+    search_alg=AxSearch(ax_client=ax, max_concurrent=2),
     verbose=2,
     resources_per_trial={"gpu": 1, "cpu": 5},
 )
