a27176eff43c5dc9dc96de7790a5a9fb16535f35
Justin Terry <justinkterry@gmail.com>
2021-03-06 21:35:34 -0500

    daemonize

--- process_hack.py
+++ process_hack.py
@@ -1,5 +1,6 @@
 import psutil
-import pickle
+import schedule
+import time
 
 """
 pids = psutil.pids()
@@ -31,11 +32,20 @@ new_old_ghosts = new_ghosts - old_ghosts
 pickle.dump(new_old_ghosts, open('/home/justin_terry/ghosts.pkl', 'wb'))
 """
 
-pids = psutil.pids()
 
-for pid in pids:
-    p = psutil.Process(pid)
-    if p.name() == 'ray::ImplicitFunc':
-        if p.memory_percent() > 0.0:
-            if p.cpu_percent(interval=1.0) == 0.0:  # slow check
-                p.kill()
+def killer():
+    pids = psutil.pids()
+
+    for pid in pids:
+        p = psutil.Process(pid)
+        if p.name() == 'ray::ImplicitFunc':
+            if p.memory_percent() > 0.0:
+                if p.cpu_percent(interval=1.0) == 0.0:  # slow check
+                    p.kill()
+
+
+schedule.every(5).seconds.do(killer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
