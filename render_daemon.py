6d3426f169e2b1e6585b170c3f77bc407f705158
Justin Terry <justinkterry@gmail.com>
2021-03-10 20:36:27 -0500

    fix scheduler

--- render_daemon.py
+++ render_daemon.py
@@ -4,23 +4,20 @@ import os
 from pathlib import Path
 
 
-class render:
-    def __init__(self):
-        self.running_policy_list = []
-        # self.max_concurrent_renders
-
-    def render(self):
-        policy_list = os.listdir(str(Path.home())+'/policies')
-        new_policies = set(self.running_policy_list)-set(policy_list)
-        self.running_policy_list = policy_list
-
-        for policy in new_policies:
-            os.system('python3 /home/justin_terry/rl_scratch/render.py '+str(Path.home())+'/'+policy)
+running_policy_list = []
 
 
-render_class = render()
+def render(self):
+    global running_policy_list
+    policy_list = os.listdir(str(Path.home())+'/policies')
+    new_policies = set(self.running_policy_list)-set(policy_list)
+    running_policy_list = policy_list
 
-schedule.every(60).seconds.do(render_class.render())
+    for policy in new_policies:
+        os.system('python3 /home/justin_terry/rl_scratch/render.py '+str(Path.home())+'/'+policy)
+
+
+schedule.every(60).seconds.do(render())
 
 while True:
     schedule.run_pending()
