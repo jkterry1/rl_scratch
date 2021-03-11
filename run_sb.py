be17322a682d342fc7c360f947e045904a1906c6
Justin Terry <justinkterry@gmail.com>
2021-03-10 21:23:05 -0500

    fix for GCP

--- run_sb.py
+++ run_sb.py
@@ -3,6 +3,7 @@ from stable_baselines import PPO2
 from pettingzoo.butterfly import pistonball_v4
 import supersuit as ss
 from stable_baselines.common.callbacks import CheckpointCallback
+from pathlib import Path
 
 env = pistonball_v4.parallel_env()
 env = ss.color_reduction_v0(env, mode='B')
@@ -12,8 +13,8 @@ env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 2, num_cpus=4, base_class='stable_baselines')
 
 
-checkpoint_callback = CheckpointCallback(save_freq=400, save_path='/home/justinkterry/logs')
-model = PPO2(CnnPolicy, env, verbose=1, gamma=.3, n_steps=75, ent_coef=.07, learning_rate=.0002, vf_coef=.5, max_grad_norm=.25, lam=.9, nminibatches=6, noptepochs=15, cliprange_vf=.15, tensorboard_log=('/home/justinkterry/tensorboard_logs/'))
+checkpoint_callback = CheckpointCallback(save_freq=400, save_path=str(Path.home())+'/logs')
+model = PPO2(CnnPolicy, env, verbose=1, gamma=.3, n_steps=75, ent_coef=.07, learning_rate=.0002, vf_coef=.5, max_grad_norm=.25, lam=.9, nminibatches=6, noptepochs=15, cliprange_vf=.15)
 model.learn(total_timesteps=6000000)
 
 
