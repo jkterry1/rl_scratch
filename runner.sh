866eeb1b143efa7a6e5a85377dcfb9f8a2ad1345
Justin Terry <justinkterry@gmail.com>
2021-03-10 21:21:45 -0500

    remove old things and rename

--- runner.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 nohup python3 run_rllib.py &> 0.out &
-CUDA_VISIBLE_DEVICES=0 nohup python3 run_rllib.py &> 1.out &
-CUDA_VISIBLE_DEVICES=1 nohup python3 run_rllib.py &> 2.out &
-CUDA_VISIBLE_DEVICES=1 nohup python3 run_rllib.py &> 3.out &
-CUDA_VISIBLE_DEVICES=2 nohup python3 run_rllib.py &> 4.out &
-CUDA_VISIBLE_DEVICES=2 nohup python3 run_rllib.py &> 5.out &
-CUDA_VISIBLE_DEVICES=3 nohup python3 run_rllib.py &> 6.out &
-CUDA_VISIBLE_DEVICES=3 nohup python3 run_rllib.py &> 7.out &
