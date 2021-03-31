26b8c313a9f8dd16a96cd522d87324af5ef22622
Justin Terry <justinkterry@gmail.com>
2021-03-31 10:49:04 -0400

    fix lunar landar test maybe

--- run_hyperparameters.py
+++ run_hyperparameters.py
@@ -105,7 +105,7 @@ def train(parameterization):
     env = make_env(parameterization['n_envs'])
     # try:
     model = PPO(CnnPolicy, env, gamma=parameterization['gamma'], n_steps=parameterization['n_steps'], ent_coef=parameterization['ent_coef'], learning_rate=parameterization['learning_rate'], vf_coef=parameterization['vf_coef'], max_grad_norm=parameterization['max_grad_norm'], gae_lam=parameterization['gae_lam'], batch_size=batch_size, n_epochs=parameterization['noptepochs'], tensorboard_log=(str(Path.home())+'/tensorboard_logs/'+name+'/'))
-    model.learn(total_timesteps=4000000, callback=checkpoint_callback)  # time steps steps of each agent
+    model.learn(total_timesteps=90000, callback=checkpoint_callback)  # time steps steps of each agent; was 4 million
     mean_reward = evaluate_all_policies(name)
     # except:
     #     mean_reward = -250
