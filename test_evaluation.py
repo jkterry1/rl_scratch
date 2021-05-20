5f44ea1b1c5c76fd5b5b99ee2cb1298eb025c068
Justin Terry <justinterry@preacherMan2.local>
2021-05-20 19:47:41 -0400

    does this work?

--- test_evaluation.py
+++ test_evaluation.py
@@ -1,9 +1,54 @@
 from stable_baselines3 import PPO
 from pettingzoo.butterfly import pistonball_v4
 import supersuit as ss
-from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage, VecNormalize
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
+
+def maybe_normalize(self, env: VecEnv, eval_env: bool) -> VecEnv:
+    """
+    Wrap the env into a VecNormalize wrapper if needed
+    and load saved statistics when present.
+
+    :param env:
+    :param eval_env:
+    :return:
+    """
+    # Pretrained model, load normalization
+    path_ = os.path.join(os.path.dirname(self.trained_agent), self.env_id)
+    path_ = os.path.join(path_, "vecnormalize.pkl")
+
+    if os.path.exists(path_):
+        print("Loading saved VecNormalize stats")
+        env = VecNormalize.load(path_, env)
+        # Deactivate training and reward normalization
+        if eval_env:
+            env.training = False
+            env.norm_reward = False
+
+    elif self.normalize:
+        # Copy to avoid changing default values by reference
+        local_normalize_kwargs = self.normalize_kwargs.copy()
+        # Do not normalize reward for env used for evaluation
+        if eval_env:
+            if len(local_normalize_kwargs) > 0:
+                local_normalize_kwargs["norm_reward"] = False
+            else:
+                local_normalize_kwargs = {"norm_reward": False}
+
+        if self.verbose > 0:
+            if len(local_normalize_kwargs) > 0:
+                print(f"Normalization activated: {local_normalize_kwargs}")
+            else:
+                print("Normalizing input and reward")
+        env = VecNormalize(env, **local_normalize_kwargs)
+    return env
+
+def image_transpose(env):
+    if is_image_space(env.observation_space) and not is_image_space_channels_first(env.observation_space):
+        env = VecTransposeImage(env)
+    return env
 
 env = pistonball_v4.parallel_env()
 env = ss.color_reduction_v0(env, mode='B')
@@ -12,6 +57,8 @@ env = ss.frame_stack_v1(env, 3)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 4, num_cpus=1, base_class='stable_baselines3')
 env = VecMonitor(env)
+env = maybe_normalize(env, eval_env=False)
+env = image_transpose(env)
 
 eval_env = pistonball_v4.parallel_env()
 eval_env = ss.color_reduction_v0(eval_env, mode='B')
@@ -20,6 +67,9 @@ eval_env = ss.frame_stack_v1(eval_env, 3)
 eval_env = ss.pettingzoo_env_to_vec_env_v0(eval_env)
 eval_env = ss.concat_vec_envs_v0(eval_env, 1, num_cpus=1, base_class='stable_baselines3')
 eval_env = VecMonitor(eval_env)
+eval_env = maybe_normalize(eval_env, eval_env=True)
+eval_env = image_transpose(eval_env)
+
 
 model = PPO("CnnPolicy", env, verbose=3, batch_size=64, n_steps=512, gamma=0.99, learning_rate=0.00018085932590331433, ent_coef=0.09728964435428247, clip_range=0.4, n_epochs=10, vf_coef=0.27344752686795376, gae_lambda=0.9, max_grad_norm=5)
 eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/', log_path='./logs/', eval_freq=int(1000), deterministic=True, render=False)
@@ -48,4 +98,4 @@ for i in range(NUM_RESETS):
 print("aec total reward: ", total_reward/NUM_RESETS)
 
 
-# OMP_NUM_THREADS=1 python3 test_evaluation.py
+# OMP_NUM_THREADS=1 nohup python3 test_evaluation.py &> test_evaluation.out &
